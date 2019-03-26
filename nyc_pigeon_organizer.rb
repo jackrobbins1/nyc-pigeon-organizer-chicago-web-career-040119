0b2504ff0e2d840eb8b09f770fe0ec85098c9966
Jack Robbins <jackrobbins1@gmail.com>
2019-03-26 16:27:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  the_pigeons = "Theo" => {
+
+                },
+                "Peter Jr." => {
+
+                },
+                "Lucky" => {
+
+                },
+                "Ms .K" => {
+
+                },
+                "Queenie" => {
+
+                },
+                "Andrew" => {
+
+                },
+                "Alex" => {
+
+                }
 end
